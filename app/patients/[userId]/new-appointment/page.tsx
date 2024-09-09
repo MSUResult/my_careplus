2c9dd1c2112c92da6dec8f78bcccbe8f58fd50fb
MSUResult <adarshrajput9040@gmail.com>
2024-09-09 08:01:55 +0530

    first commit

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import PatientForm from "@/components/forms/PatientForm";
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+import * as Sentry from '@sentry/nextjs';
+
+export default async function NewAppointment({ params: { userId } }: SearchParamProps) {
+  const patient = await getPatient(userId);
+
+  Sentry.metrics.set("user_view_new-appointment", patient.name);
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="mb-12 h-10 w-fit"
+          />
+
+          {/* Since setOpen is not needed in the server component context, it's removed */}
+          <AppointmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
+
+          <p className="copyright mt-10 py-12">© 2024 CarePluse</p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1500}
+        width={1500}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://745dee2441693e686c40d29fe390cc39@o4507917703774208.ingest.us.sentry.io/4507917708361728",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
